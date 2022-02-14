efac6a4c7f0636f56a85277fa62f4317850f45f6
dobbymatrix <bhan.universe@gmail.com>
2022-02-14 19:53:58 +0900

    Choropleth path css updated, highlighted updated

--- client/src/components/CovidMap.js
+++ client/src/components/CovidMap.js
@@ -6,6 +6,7 @@ import './CovidMap.css'
 
 const CovidMap = ({ eventData }) => {
   const [countries, setCountries] = useState([])
+  // const [onselect, setOnselect] = useState({})
 
   const globalData = eventData[eventData.length - 1]
 
@@ -14,22 +15,27 @@ const CovidMap = ({ eventData }) => {
     loadCountriesTask.loadMapData(globalData, setCountries)
   }
 
-  const highlight = (e) => {
+  const highlightFeature = (e) => {
     var layer = e.target
+    const { ADMIN, confirmed } = layer.feature.properties
+
+    // setOnselect({
+    //   country: ADMIN,
+    //   confirmed: confirmed,
+    // })
+
     layer.setStyle({
-      dashArray: '2',
-      weight: 3,
+      weight: 5,
       color: 'var(--clr-white)',
+      dashArray: '',
+      fillOpacity: 0.7
     })
+
   }
 
-  const unhighlight = (e) => {
-    var layer = e.target
-    layer.setStyle({
-      weight: 1,
-      dashArray: '',
-      color: 'var(--clr-grey-border)',
-    })
+  const resetHighlight = (e) => {
+    // setOnselect({})
+    e.target.setStyle(style(e.target.feature))
   }
 
   const onEachCountry = (country, layer) => {
@@ -38,34 +44,54 @@ const CovidMap = ({ eventData }) => {
     const confirmedText = country.properties.confirmedText
     layer.bindPopup(`${name} ${confirmedText}`)
     layer.on({
-      mouseover: highlight,
-      mouseout: unhighlight,
+      mouseover: highlightFeature,
+      mouseout: resetHighlight,
     })
   }
 
-  const mapStyle = {
-    fillColor: 'var(--clr-white)',
-    weight: 1,
-    color: 'var(--clr-grey-border)',
-    fillOpacity: 1,
+  const style = (feature) => {
+    return {
+      fillColor: feature.properties.color,
+      weight: 0.5,
+      opacity: 1,
+      color: 'var(--clr-grey-border)',
+      fillOpacity: 1,
+    }
   }
 
   useEffect(load, [])
 
   return (
-    <MapContainer
-      style={{ height: '40rem' }}
-      zoom={2}
-      maxZoom={5}
-      center={[20, 100]}
-    >
-      <GeoJSON
-        style={mapStyle}
-        data={countries}
-        onEachFeature={onEachCountry}
-        attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
-      />
-    </MapContainer>
+    <>
+      <MapContainer
+        style={{ height: '55vh' }}
+        zoom={2}
+        maxZoom={5}
+        center={[20, 100]}
+      >
+        <GeoJSON
+          data={countries}
+          style={style}
+          onEachFeature={onEachCountry}
+          attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
+        />
+      </MapContainer>
+      {/* {!onselect.country && (
+        <div className='census-info-hover'>
+          <strong>확진자 수</strong>
+          <p>?</p>
+        </div>
+      )}
+      {onselect.country && (
+        <ul className='census-info'>
+          <li>
+            <strong>{onselect.country}</strong>
+          </li>
+          <br />
+          <li>Total Population:{onselect.confirmed}</li>
+        </ul>
+      )} */}
+    </>
   )
 }
 
--- client/src/entities/LegendItems.js
+++ client/src/entities/LegendItems.js
@@ -3,34 +3,54 @@ import LegendItem from './LegendItem'
 var legendItems = [
   new LegendItem(
     '10,000,000 +',
-    'var(--clr-orange-5)',
+    'var(--clr-orange-8)',
     (cases) => cases >= 10_000_000,
     'var(--clr-white)'
   ),
 
   new LegendItem(
     '5,000,000 - 9,999,999',
-    'var(--clr-orange-4)',
+    'var(--clr-orange-7)',
     (cases) => cases >= 5_000_000 && cases < 10_000_000,
     'var(--clr-white)'
   ),
 
   new LegendItem(
     '1,000,000 - 4,999,999',
-    'var(--clr-orange-3)',
-    (cases) => cases >= 1_000_000 && cases < 5_000_000
+    'var(--clr-orange-6)',
+    (cases) => cases >= 1_000_000 && cases < 5_000_000,
+    'var(--clr-white)'
   ),
 
   new LegendItem(
     '500,000 - 999,999',
-    'var(--clr-orange-2)',
-    (cases) => cases >= 500_000 && cases < 1_000_000
+    'var(--clr-orange-5)',
+    (cases) => cases >= 500_000 && cases < 1_000_000,
+    'var(--clr-white)'
   ),
 
   new LegendItem(
-    '0 - 499,999',
+    '250,000 - 499,999',
+    'var(--clr-orange-4)',
+    (cases) => cases >= 250_000 && cases < 500_000
+  ),
+
+  new LegendItem(
+    '100,000 - 249,999',
+    'var(--clr-orange-3)',
+    (cases) => cases >= 100_000 && cases < 250_000
+  ),
+
+  new LegendItem(
+    '50,000 - 99,999',
+    'var(--clr-orange-2)',
+    (cases) => cases >= 50_000 && cases < 100_000
+  ),
+
+  new LegendItem(
+    '0 - 49,999',
     'var(--clr-orange-1)',
-    (cases) => cases > 0 && cases < 500_000
+    (cases) => cases > 0 && cases < 50_000
   ),
 
   new LegendItem('No Data', 'var(--clr-white)', (cases) => true),
